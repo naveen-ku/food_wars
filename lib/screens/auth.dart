9c8e3689982efd7f545756402365062ad0a1ad41
Naveen Kumar <naveen2951996@gmail.com>
2020-10-30 18:18:24 +0530

    Ui (#4)
    * Adding basic auth page
    
    * Fixing Image
    
    * Outsourcing code and completing auth page
    
    * Adding code to app architecture

---
+++ lib/screens/auth.dart
@@ -0,0 +1,170 @@
+import "package:flutter/material.dart";
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:food_wars/helper/dimensions.dart';
+import 'package:food_wars/screens/landing_screen.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  Map<String, dynamic> _formData = {
+    'nameValue': null,
+    'emailValue': null,
+    'passValue': null,
+  };
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  DecorationImage _buildBackgroundImage() {
+    return DecorationImage(
+      fit: BoxFit.cover,
+      colorFilter:
+          ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.dstATop),
+      image: AssetImage('assets/images/food_bg.jpg'),
+    );
+  }
+
+  Widget _buildNameTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Name',
+      ),
+      keyboardType: TextInputType.name,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Name is required';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _formData['nameValue'] = value;
+      },
+    );
+  }
+
+  Widget _buildEmailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Email'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Email is required';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _formData['emailValue'] = value;
+      },
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+      ),
+      obscureText: true,
+      //controller: _passwordTextController,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Password is required';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _formData['passValue'] = value;
+      },
+    );
+  }
+
+  Widget _switchSignupModeButton() {
+    return FlatButton(
+      child: Text(
+        'Switch to Signup',
+        style: TextStyle(color: Colors.black),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget _signinButton() {
+    return Center(
+      child: RaisedButton(
+        color: Theme.of(context).primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40.0),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+        child: Text(
+          "Sign in",
+          style:
+              TextStyle(color: Theme.of(context).accentColor, fontSize: 20.0),
+        ),
+        onPressed: () => _submitForm(),
+      ),
+    );
+  }
+
+  void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return LandingScreen();
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var vpW = getViewportWidth(context);
+    var vpH = getViewportHeight(context);
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          image: _buildBackgroundImage(),
+        ),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Container(
+                width: vpW * .90,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: vpH * .125),
+                    _buildNameTextField(),
+                    SizedBox(height: vpH * .025),
+                    _buildEmailTextField(),
+                    SizedBox(height: vpH * .025),
+                    _buildPasswordTextField(),
+                    SizedBox(height: vpH * .025),
+                    _switchSignupModeButton(),
+                    _signinButton(),
+                    SizedBox(height: vpH * .1),
+                    SignInButton(
+                      Buttons.FacebookNew,
+                      onPressed: () {},
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    SignInButton(
+                      Buttons.Google,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,71 +1,80 @@
 import "package:flutter/material.dart";
+import 'package:food_wars/screens/auth.dart';
 import 'package:food_wars/helper/dimensions.dart';
 
 class LandingScreen extends StatelessWidget {
   Widget _buildImage() {
-    return Transform.rotate(
-      angle: -50 * 3.14 / 180,
-      child: Container(
+    return Container(
+        // alignment: Alignment.bottomCenter,
         margin: EdgeInsets.only(bottom: 30.0),
         child: Image.asset('assets/images/food.png'),
-        color: Colors.transparent,
+        color: Colors.transparent);
+  }
+
+  Widget _buildMainHeading(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      child: Center(
+        child: Text(
+          "WELCOME TO FOOD WARS",
+          style: TextStyle(
+              fontSize: 26.0,
+              color: Theme.of(context).primaryColor,
+              fontWeight: FontWeight.bold),
+        ),
       ),
+    );
+  }
+
+  Widget _buildSubHeading(var vpH, BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10.0, bottom: 20.0),
+      child: Text(
+        "Find your favourite food stall right away",
+        style: TextStyle(
+          fontSize: vpH * 0.020,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGetStartedButton(var vpH, BuildContext context) {
+    return RaisedButton(
+      color: Theme.of(context).primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(40.0),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Text(
+        "Get Started",
+        style: TextStyle(color: Theme.of(context).accentColor, fontSize: 20.0),
+      ),
+      onPressed: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (BuildContext context) {
+          return AuthScreen();
+        }));
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    // final double deviceHight = MediaQuery.of(context).size.height;
     var vpW = getViewportWidth(context);
     var vpH = getViewportHeight(context);
 
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          child: Column(
-            children: [
-              SizedBox(
-                height: vpH * 0.025,
-              ),
-              _buildImage(),
-              SizedBox(height: vpH * 0.025),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: Center(
-                  child: Text(
-                    "WELCOME TO FOOD WARS",
-                    style: TextStyle(
-                        fontSize: vpH * 0.035,
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 10.0, bottom: 20.0),
-                child: Text(
-                  "Find your favourite food stall right away",
-                  style: TextStyle(
-                    fontSize: vpH * 0.023,
-                  ),
-                ),
-              ),
-              RaisedButton(
-                color: Theme.of(context).primaryColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40.0),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                child: Text(
-                  "Get Started",
-                  style: TextStyle(
-                    color: Theme.of(context).accentColor,
-                    fontSize: vpH * 0.035,
-                  ),
-                ),
-                onPressed: () {},
-              ),
-            ],
-          ),
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(height: vpH * .15),
+            _buildImage(),
+            SizedBox(height: vpH * 0.025),
+            _buildMainHeading(context),
+            _buildSubHeading(vpH, context),
+            _buildGetStartedButton(vpH, context),
+          ],
         ),
       ),
     );
